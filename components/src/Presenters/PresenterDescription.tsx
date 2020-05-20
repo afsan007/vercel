e3f30e3f0a7dbb6f35f4e6fac7a3b3009443870c
Narges Haghighi <narges.haghighi@avidarvand.com>
2020-05-20 08:28:15 +0000

    Edit presenter description section

--- components/src/Presenters/PresenterDescription.tsx
+++ components/src/Presenters/PresenterDescription.tsx
@@ -1,14 +1,11 @@
 import React, { FC } from "react";
-import Grid from "@material-ui/core/Grid";
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
+import { Grid, Avatar, Typography, Button } from "@material-ui/core";
 import { ShowMoreText } from "../Text/ShowMore";
-import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 
 const DescriptionDiv = styled(Grid)`
   padding: 26px 68px 34px 50px;
-  max-width: 850px;
+  width: 850px;
   min-height: 301px;
   border-radius: 15px;
   box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
@@ -19,9 +16,14 @@ const DescriptionDiv = styled(Grid)`
   }
 `;
 
+const PresenterDetail = styled(Grid)`
+  margin-bottom: 5px;
+  margin-top: 5px;
+`;
+
 const PresenterName = styled(Typography)`
   font-family: "IRANSans";
-  margin-bottom: 10px;
+  margin-bottom: 15px;
   @media (max-width: 768px) {
     text-align: center;
   }
@@ -61,6 +63,7 @@ const Education = styled.span`
 const PresenterDesc = styled.div`
   font-family: "IRANSans";
   min-height: 150px;
+
   margin-top: 25px;
   font-size: 16px;
   font-weight: normal;
@@ -79,7 +82,9 @@ const FollowButton = styled(Button)`
   font-family: "IRANSans";
   color:#ffffff
   font-size: 14px;
+  margin: 0 auto;
   margin-top:8px;
+  text-align: center;
 `;
 
 export interface PresenterDescriptionProps {
@@ -102,60 +107,64 @@ export const PresenterDescription: FC<PresenterDescriptionProps> = (props) => {
   }
 
   return (
-    <DescriptionDiv
-      container
-      direction="row"
-      justify="flex-start"
-      alignItems="flex-start"
-    >
-      <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
-        <FatherGrid
-          container
-          direction="row"
-          justify="center"
-          alignItems="flex-end"
-        >
-          <Grid item lg={2} xl={3} md={3} sm={3} xs={12}>
-            <PresenterImage
-              src={props.prsenterImage}
-              title={props.prsenterName}
-            />
-          </Grid>
-          <Grid item lg={3} xl={5} md={2} sm={2} xs={12}>
-            <FatherGrid
-              item
-              xs={12}
-              container
-              direction="column"
-              justify="flex-start"
-              alignItems="center"
-            >
-              <Grid item xs={10} lg={11} xl={11} md={11} sm={10}>
-                <PresenterName variant="h6" gutterBottom>
-                  {props.prsenterName}
-                </PresenterName>
-              </Grid>
-              <Grid item xs={10} lg={11} xl={11} md={11} sm={10}>
-                <Title>دانشگاه:</Title>{" "}
-                <Education>{props.prsenterUniversity}</Education>
-              </Grid>
-              <div>
-                <Title>رشته:</Title>{" "}
-                <Education>{props.prsenterEducation}</Education>
-              </div>
-            </FatherGrid>
-          </Grid>
-          <Grid item lg={3} xl={1} md={2} sm={4}></Grid>
-          <FollowGrid item lg={4} xl={4} md={5} sm={3} xs={12}>
-            <FollowButton>دنبال کردن</FollowButton>
-          </FollowGrid>
-        </FatherGrid>
-      </Grid>
-      <Grid item lg={12} xl={12} md={12} sm={12} xs={12}>
-        <PresenterDesc>
-          <ShowMoreText text={props.description} />
-        </PresenterDesc>
-      </Grid>
+    <DescriptionDiv>
+      <FatherGrid
+        item
+        xs={12}
+        container
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-end"
+      >
+        <Grid item lg={5} xl={5} md={5} sm={12} xs={12}>
+          <FatherGrid
+            item
+            xs={12}
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-end"
+          >
+            <Grid item lg={6} xl={6} md={6} sm={12} xs={12}>
+              <PresenterImage
+                src={props.prsenterImage}
+                title={props.prsenterName}
+              />
+            </Grid>
+            <Grid item lg={6} xl={6} md={6} sm={12} xs={12}>
+              <PresenterDetail
+                item
+                xs={12}
+                container
+                direction="column"
+                justify="flex-start"
+                alignItems="center"
+              >
+                <Grid item xs={12} lg={12} xl={12} md={12} sm={12}>
+                  <PresenterName variant="h6" gutterBottom>
+                    {props.prsenterName}
+                  </PresenterName>
+                </Grid>
+                <Grid item xs={12} lg={12} xl={12} md={12} sm={12}>
+                  <Title>دانشگاه:</Title>{" "}
+                  <Education>{props.prsenterUniversity}</Education>
+                </Grid>
+                <Grid item xs={12} lg={12} xl={12} md={12} sm={12}>
+                  <Title>رشته:</Title>{" "}
+                  <Education>{props.prsenterEducation}</Education>
+                </Grid>
+              </PresenterDetail>
+            </Grid>
+          </FatherGrid>
+        </Grid>
+        <Grid item lg={2} xl={2} md={2} />
+        <FollowGrid item lg={5} xl={5} md={5} sm={12} xs={12}>
+          <FollowButton>دنبال کردن</FollowButton>
+        </FollowGrid>
+      </FatherGrid>
+      <PresenterDesc>
+        <ShowMoreText text={props.description} />
+      </PresenterDesc>
     </DescriptionDiv>
   );
 };
