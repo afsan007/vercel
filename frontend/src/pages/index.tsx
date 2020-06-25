db1a01387babc0f601c3cea486ed011b02c42d10
Afsan <af.hadafi@gmail.com>
2020-06-25 12:16:52 +0430

    alltt

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,9 @@ import { NextPage } from 'next'
 // import Page from '$components/layout/Page';
 
 const Home: NextPage<{ test: string }> = () => {
-	return <h1 className='container'>Welcome to NextJS</h1>
+	return (
+		<h1 className='container'>Welcome to NextJS commit 2</h1>
+	)
 }
 
 export default Home
